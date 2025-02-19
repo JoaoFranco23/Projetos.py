44e9e7b96b67490079dee138d97f9c128af091ad
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2025-02-19 00:11:32 -0300

    Update Login.py

--- Login.py
+++ Login.py
@@ -12,9 +12,10 @@ class Application():
     def __init__(self):
         self.Login = Login
         self.tela()
+        self.button()
         Login.mainloop()
         
-    def tela(self):#Esta funçao, estou definindo o tamanho da dimensão que a tela sera iniciada
+    def tela(self):#Esta funçao, estou configurando o tamanho da dimensão que a tela sera iniciada
         self.Login.title("Login")
         self.Login.configure(background='lightblue')#Configuração da tela e cores do fundo
         self.Login.geometry("600x500")#Definindo o Tamanho da tela
@@ -22,10 +23,19 @@ class Application():
         self.Login = Label(Login, text="Login", background='lightblue', font=("Arial", 20)).pack() #Nesta variavel, definimos um titulo dentro do card
         
         #Frame para criarmos o card, login de acesso do usuario 
-        Email = Frame(Login, bg="lightblue", bd=2, relief="groove")
+        #Email = Label(Login, text="Email", anchor=NW, font=('arial 10'))
+        Email = Frame(Login, bg="lightblue", bd=1, relief="groove")
         Email = Entry(Login, textvariable= "Informe o email")
         Email.place(relx=0.2, rely=0.2, relwidth=0.6, relheight=0.07)
+        pass
         
-        def frame_senha_tela(self):
-            
+        Senha = Frame(Login, bg="lightblue", bd=2, relief="groove", highlightthickness=1)
+        Senha = Entry(Login, show="*", bd=1, width=25, justify='left', font=("", 15), highlightthickness=1,  relief="solid")
+        Senha.place(y=2, x=2, relx=0.5, rely= 0.5, relwidth=0.6, relheight=0.07, anchor="center") 
+        
+    def button(self):
+        self.btn = Frame(Login, bd=2, highlightthickness=1)
+        self.btn = Button(self.Login, text="ENTRAR")
+        self.btn.place(relx=0.5, rely=0.9, anchor="center", relwidth=0.3, width=55)
+        
 Application()
